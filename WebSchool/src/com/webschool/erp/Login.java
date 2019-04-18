d172271e7039b88ead6f9fb34c9c5f32fdfc4bb3
vinay kumar <namagondlavinay@gmail.com>
2019-04-18 10:55:01 +0530

    this is second commit

--- WebSchool/src/com/webschool/erp/Login.java
+++ WebSchool/src/com/webschool/erp/Login.java
@@ -4,6 +4,8 @@ public class Login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//this is my second commit
+		System.out.println("this is webschool ERP");
 
 	}
 
